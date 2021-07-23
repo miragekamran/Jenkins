7767280cc2a45a9fdbcd53d4dd27ef3af3dd9288
mirage <mirage.kamran.pkt@gmail.com>
2021-07-22 18:49:00 -0700

    3rd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-	    for (int i = 0; i < 10; i++) {
+	    for (int i = 0; i < 8; i++) {
             System.out.println("I am going to get a QA job soon");
             Thread.sleep(1000);
         }
